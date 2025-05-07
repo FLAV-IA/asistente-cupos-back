99b76b74d06955405747dc848bb772a58bc88b64
Marcos Alvarenga <alvarenga.marcos.daniel@gmail.com>
2025-05-07 17:50:36 -0300

    Excepciones custom

---
+++ src/main/java/com/edu/asistenteCupos/excepcion/ComisionesDeDistintaMateriaException.java
@@ -0,0 +1,11 @@
+package com.edu.asistenteCupos.excepcion;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class ComisionesDeDistintaMateriaException extends RuntimeException {
+    public ComisionesDeDistintaMateriaException(String mensaje) {
+        super(mensaje);
+    }
+}
