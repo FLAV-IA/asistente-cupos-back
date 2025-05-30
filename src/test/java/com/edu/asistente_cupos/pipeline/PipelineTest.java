3ae3c94f06a4841444f5a1385dd5008de6784647
Marcos Alvarenga <alvarenga.marcos.daniel@gmail.com>
2025-05-30 06:30:21 -0300

    Documenta los pasos específicos omitiendo el orquestador del pipeline que se testea en otro lado

--- src/test/java/com/edu/asistente_cupos/pipeline/PipelineTest.java
+++ src/test/java/com/edu/asistente_cupos/pipeline/PipelineTest.java
@@ -2,7 +2,9 @@ package com.edu.asistente_cupos.pipeline;
 
 
 import com.edu.asistente_cupos.config.MockConfig;
-import com.edu.asistente_cupos.service.AsistenteDeInscripcion;
+import com.edu.asistente_cupos.domain.peticion.PeticionInscripcion;
+import com.edu.asistente_cupos.domain.priorizacion.PeticionPorMateriaPriorizada;
+import com.edu.asistente_cupos.domain.sugerencia.SugerenciaInscripcion;
 import com.edu.asistente_cupos.service.llm.LlmClient;
 import com.edu.asistente_cupos.testutils.TestDataFactory;
 import org.junit.jupiter.api.Test;
@@ -28,12 +30,16 @@ class PipelineTest {
   @Container
   protected static final MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0")
     .withDatabaseName("asistente").withUsername("root").withPassword("test");
-
   @Autowired
   private LlmClient llmClient;
-
   @Autowired
-  private AsistenteDeInscripcion asistente;
+  private Paso<List<PeticionInscripcion>, List<PeticionInscripcion>> filtroPaso;
+  @Autowired
+  private Paso<List<PeticionInscripcion>, List<PeticionPorMateriaPriorizada>> priorizacionPaso;
+  @Autowired
+  private Paso<List<PeticionPorMateriaPriorizada>, List<SugerenciaInscripcion>> asignacionPaso;
+  @Autowired
+  private Paso<List<SugerenciaInscripcion>, List<SugerenciaInscripcion>> traduccionYConversorPaso;
 
   @DynamicPropertySource
   static void registrarPropiedades(DynamicPropertyRegistry registry) {
@@ -46,7 +52,15 @@ class PipelineTest {
   @Test
   void ejecutaTodoElPipelineConBeansRealesYMockDeLlm() {
     var peticiones = List.of(TestDataFactory.crearPeticionInscripcionParaPipeline());
-    var resultado = asistente.sugerirInscripcion(peticiones);
+
+    var filtradas = filtroPaso.ejecutar(peticiones);
+
+    var priorizadas = priorizacionPaso.ejecutar(filtradas);
+
+    var sugerencias = asignacionPaso.ejecutar(priorizadas);
+
+    var resultado = traduccionYConversorPaso.ejecutar(sugerencias);
+
     assertThat(resultado).isNotEmpty();
   }
 }
